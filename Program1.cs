47f469d1bca5d93b0372bbcc5c7fcbc3ff2392d4
Anjali1962004 <sharmaanjali1962004@gmail.com>
2025-10-24 22:45:47 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int sum=0;
+        foreach(String arg in args) { 
+            sum=sum+ int.Parse(arg);
+        }
+        Console.WriteLine(sum);
+    }
+}
+
